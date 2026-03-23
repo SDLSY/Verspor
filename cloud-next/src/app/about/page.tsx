862f17200f2eda97d0be530fcb3f1f03940eec0b
lishangyi <3067217117@qq.com>
2026-03-23 12:18:05 +0800

    feat: serve stitch marketing pages

--- cloud-next/src/app/about/page.tsx
+++ cloud-next/src/app/about/page.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "next";
 import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  redirect("/stitch/home.html" as Route);
+export default function AboutPage() {
+  redirect("/stitch/about.html" as Route);
 }
